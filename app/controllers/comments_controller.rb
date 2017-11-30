4c4f779bf06d49c866b11d9b258dfda810ee457b
ion <ion.sirbu.91@gmail.com>
2017-12-01 00:00:56 +0100

    little bug in edit comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,26 +14,30 @@ class CommentsController < ApplicationController
   end
 
   # GET /comments/new
+  #new works fine
   def new
     @comment = Comment.new
   end
 
   # GET /comments/1/edit
   def edit
-    @comment.commented_id = params[:gamer_id]
+    @comment.update(comment_params)
+
+=begin
     respond_to do |format|
-      if @comment.update(comment_params)
-        format.html { redirect_to edit_gamer_comment_path, notice: 'Comment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @comment }
+      if !@edited.save
+        format.html { redirect_to gamer_comments_path, notice: 'Comment was successfully updated.' }
+        format.json { render :show, status: :created, location: @comment }
       else
-        format.html { render :edit }
+        format.html { render :new }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
-    end
+=end
   end
 
   # POST /comments
   # POST /comments.json
+  #create works fine
   def create
     @comment = Comment.new(comment_params)
     @comment.commented_id = params[:gamer_id]
@@ -53,34 +57,24 @@ class CommentsController < ApplicationController
   # PATCH/PUT /comments/1
   # PATCH/PUT /comments/1.json
   def update
-    @comment.commented_id = params[:gamer_id]
-    respond_to do |format|
-      if @comment.update(comment_params)
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @comment }
-      else
-        format.html { render :edit }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
+    @comment.mark=comment_params.mark
+    @comment.review=comment_params.review
   end
 
   # DELETE /comments/1
   # DELETE /comments/1.json
-
+  #destroy works fine
 def destroy
     @comment.destroy
-    redirect_to gamers_path
-=begin
     respond_to do |format|
-      format.html { redirect_to gamers_path, notice: 'Comment was successfully destroyed.' }
+      format.html { redirect_to gamer_path(params[:gamer_id]), notice: 'Comment was successfully destroyed.' }
       format.json { head :no_content }
     end
-=end
+
   end
 
 
-  private
+private
     # Use callbacks to share common setup or constraints between actions.
     def set_comment
       @comment = Comment.find_by("commentator_id=?",current_user)
