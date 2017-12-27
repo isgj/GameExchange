7b278bd94333e13ce055b72fbe2a104c5e7ed30a
Sby-EasyStuff <manuel.mancini@gmail.com>
2017-12-27 16:39:51 +0100

    Added homepage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,21 @@
 class WelcomeController < ApplicationController
+  before_action :get_gamers
+  before_action :get_games
+  before_action :get_titles
+
   def index
   end
+
+  private
+    def get_gamers
+      @gamers = User.order("id DESC").limit(3)
+    end
+
+    def get_games
+      @games = Game.order("id DESC").limit(6)
+    end
+
+    def get_titles
+      @titles = GameInfo.order("id DESC").limit(6)
+    end
 end
