cdfa128b0cc788d8022be99c2cfa124e46ba1eaa
isgj <isgjevori@gmail.com>
2018-04-27 16:39:20 +0200

    Handle error same email

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,10 +4,17 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-      sign_in_and_redirect @user  #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => provider.capitalize) if is_navigational_format?
+      begin
+        sign_in_and_redirect @user  #this will throw if @user is not activated
+        set_flash_message(:notice, :success, :kind => provider.capitalize) if is_navigational_format?
+      rescue => e
+        logger.error "Rescued from #{e.inspect}, authentication from #{provider}"
+        set_flash_message(:notice, :success, :kind => "Error while authenticating")
+        redirect_to new_user_registration_url
+      end
     else
       session["devise.#{provider}_data"] = request.env["omniauth.auth"]
+      set_flash_message(:notice, :success, :kind => "Error while authenticating")
       redirect_to new_user_registration_url
     end
   end
